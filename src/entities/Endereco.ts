90b4d2b268272fba661cb6dc6469a6f41e1d3be2
Thiago Pereira <titiura@gmail.com>
2024-07-16 09:44:34 -0300

    feat: create address entity

---
+++ src/entities/Endereco.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export default class EnderecoEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  @Column()
+  cidade: string;
+  @Column()
+  estado: string;
+
+  constructor(
+    cidade: string,
+    estado: string,
+
+  ) {
+    this.cidade = cidade;
+    this.estado = estado;
+  }
+}
