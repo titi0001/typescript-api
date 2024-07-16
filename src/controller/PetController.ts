0dbbdb55ab755b9d0a043e5b545ae9d929806de4
Thiago Pereira <titiura@gmail.com>
2024-07-16 17:03:29 -0300

    feat: add type req , res to controller

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,42 +1,65 @@
 import { Request, Response } from "express";
-import TipoPet from "../types/typePet";
 import EnumEspecie from "../enum/EnumEspecie";
 import PetRepository from "../repositories/PetRepository";
 import PetEntity from "../entities/PetEntity";
 import EnumPorte from "../enum/EnumPorte";
-
-let listaDePets: Array<TipoPet> = [];
-let id = 0;
-function gerarId(): number {
-  return ++id;
-}
+import {
+  TipoRequestBodyPet,
+  TipoRequestParamsPet,
+  TipoResponseBodyPet,
+} from "../types/tiposPets";
 
 export default class PetController {
   constructor(private repository: PetRepository) {}
 
-  async criaPet(req: Request, res: Response) {
-    const { adotado, especie, dataDeNascimento, nome, porte } = <PetEntity>req.body;
+  async criaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
+    const { adotado, especie, dataDeNascimento, nome, porte } = <PetEntity>(
+      req.body
+    );
 
     if (!Object.values(EnumEspecie).includes(especie)) {
-      return res.status(400).json({ message: "Espécie inválida" });
+      return res.status(400).json({ error: "Espécie inválida" });
     }
 
     if (porte && !(porte in EnumPorte)) {
-      return res.status(400).json({ message: "Porte inválido" });
+      return res.status(400).json({ error: "Porte inválido" });
     }
 
-    const novoPet = new PetEntity(nome, especie, dataDeNascimento, adotado, porte);
+    const novoPet = new PetEntity(
+      nome,
+      especie,
+      dataDeNascimento,
+      adotado,
+      porte
+    );
 
     await this.repository.criaPet(novoPet);
-    return res.status(201).json(novoPet);
+    return res
+      .status(201)
+      .json({ data: { id: novoPet.id, nome, especie, porte } });
   }
 
-  async listaPets(req: Request, res: Response) {
+  async listaPets(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
     const listaDePets = await this.repository.listaPets();
-    return res.status(200).json(listaDePets);
+    const data = listaDePets.map(({ id, nome, especie, porte }) => ({
+      id,
+      nome,
+      especie,
+      porte,
+    }));
+    return res.status(200).json({ data });
   }
 
-  async atualizaPet(req: Request, res: Response) {
+  async atualizaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
     const { id } = req.params;
     const { success, message } = await this.repository.atualizaPet(
       Number(id),
@@ -44,42 +67,48 @@ export default class PetController {
     );
 
     if (!success) {
-      return res.status(404).json({ message });
+      return res.status(404).json({ error: message });
     }
     return res.status(204);
   }
 
-   async deletaPet(req: Request, res: Response) {
+  async deletaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
     const { id } = req.params;
 
-    const { success, message } =  await this.repository.deletaPet(Number(id));
+    const { success, message } = await this.repository.deletaPet(Number(id));
 
     if (!success) {
-      return res.status(404).json({ message });
+      return res.status(404).json({ error: message });
     }
     return res.sendStatus(204);
   }
 
-  async adotaPet(req: Request, res: Response) {
+  async adotaPet(
+    req: Request<TipoRequestParamsPet, {}, TipoRequestBodyPet>,
+    res: Response<TipoResponseBodyPet>
+  ) {
     const { pet_id, adotante_id } = req.params;
 
-    const { success, message } = await this.repository.adotaPet(	
-      Number(pet_id),	
-      Number(adotante_id)	
-    ); 
+    const { success, message } = await this.repository.adotaPet(
+      Number(pet_id),
+      Number(adotante_id)
+    );
 
     if (!success) {
-      return res.status(404).json({ message });
+      return res.status(404).json({ error: message });
     }
     return res.sendStatus(204);
-  } 
+  }
 
   async bucaPetPorCampoGenerico(req: Request, res: Response) {
     const { campo, valor } = req.query;
-    const listaDePets = await this.repository.buscaPetPorCampoGenerico(campo as keyof PetEntity, 
-    valor as string
+    const listaDePets = await this.repository.buscaPetPorCampoGenerico(
+      campo as keyof PetEntity,
+      valor as string
     );
-  return res.status(200).json(listaDePets);  
+    return res.status(200).json(listaDePets);
   }
 }
-
