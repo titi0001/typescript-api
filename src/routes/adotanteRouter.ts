5248d8868bb111533fa4b730523dc72799856eaf
Thiago Pereira <titiura@gmail.com>
2024-07-17 12:21:07 -0300

    feat: add middleware to router  post

--- src/routes/adotanteRouter.ts
+++ src/routes/adotanteRouter.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { AppDataSource } from "../config/dataSource";
 import AdotanteController from "../controller/AdotanteController";
 import AdotanteRepository from "../repositories/AdotanteRepository";
+import { middlewareValidadorBodyAdotante } from "../middleware/validadores/adotanteRequestBody";
+import { RequestHandler } from "express-serve-static-core";
 
 const router = express.Router();
 
@@ -10,8 +12,9 @@ const adotanteRepository = new AdotanteRepository(
 );
 
 const adotanteController = new AdotanteController(adotanteRepository);
+const validateBody:RequestHandler = (req, res, next) => middlewareValidadorBodyAdotante(req, res, next)
 
-router.post("/", (req, res) => adotanteController.criaAdotante(req, res));
+router.post("/", validateBody , (req, res) => adotanteController.criaAdotante(req, res));
 router.get("/", (req, res) => adotanteController.listaAdotantes(req, res));
 router.put("/:id", (req, res) => adotanteController.atualizaAdotante(req, res));
 router.delete("/:id", (req, res) => adotanteController.deletaAdotante(req, res));
