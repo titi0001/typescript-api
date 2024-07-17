111c1129d9b79dbf021c8bc91582ead70655eaef
Thiago Pereira <titiura@gmail.com>
2024-07-17 13:33:39 -0300

    feat: create middleware validator endereco

---
+++ src/middleware/validadores/enderecoRequestBody.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import * as yup from 'yup';
+import EnderecoEntity from '../../entities/Endereco';
+
+const esquemaBodyEndereco: yup.ObjectSchema<Omit<EnderecoEntity, "id">> = yup.object({
+  cidade: yup.string().defined().required(),
+  estado: yup.string().defined().required(),
+});
+
+const middlewareValidadorBodyEndereco = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await esquemaBodyEndereco.validate(req.body, { abortEarly: false });
+    return next();
+    
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    const validationErros: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if(!error.path) return;
+
+      validationErros[error.path] = error.message;
+    });
+
+    return res.status(400).json({ error: validationErros });
+  }
+}
+
+export { middlewareValidadorBodyEndereco };
