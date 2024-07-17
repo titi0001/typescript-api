b92dfa68f1d1dcb0aa4af1a11bb4f1814548d535
Thiago Pereira <titiura@gmail.com>
2024-07-17 15:19:44 -0300

    feat: add yup set locale pt

--- src/middleware/validadores/adotanteRequestBody.ts
+++ src/middleware/validadores/adotanteRequestBody.ts
@@ -1,33 +1,47 @@
-import { Request, Response, NextFunction } from 'express';
-import * as yup from 'yup';
-import { TipoRequestBodyAdotante } from '../../types/tiposAdotante';
+import { Request, Response, NextFunction } from "express";
+import * as yup from "yup";
+import { pt } from "yup-locale-pt";
+import { TipoRequestBodyAdotante } from "../../types/tiposAdotante";
 
-const esquemaBodyAdotante: yup.ObjectSchema<Omit<TipoRequestBodyAdotante, "endereco">> = yup.object({
+yup.setLocale(pt);
+
+const esquemaBodyAdotante: yup.ObjectSchema<
+  Omit<TipoRequestBodyAdotante, "endereco">
+> = yup.object({
   nome: yup.string().defined().required(),
-  celular: yup.string().defined().required()
-  .matches(/^(\(?[0-9]{2}\)?)? ?([0-9]{4,5})-?([0-9]{4})$/gm, "Celular inválido"),
+  celular: yup
+    .string()
+    .defined()
+    .required()
+    .matches(
+      /^(\(?[0-9]{2}\)?)? ?([0-9]{4,5})-?([0-9]{4})$/gm,
+      "Celular inválido"
+    ),
   senha: yup.string().defined().required().min(6),
-  foto: yup.string().optional() ,
+  foto: yup.string().optional(),
 });
 
-const middlewareValidadorBodyAdotante = async (req: Request, res: Response, next: NextFunction) => {
+const middlewareValidadorBodyAdotante = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     await esquemaBodyAdotante.validate(req.body, { abortEarly: false });
     return next();
-    
   } catch (error) {
     const yupError = error as yup.ValidationError;
 
     const validationErros: Record<string, string> = {};
 
     yupError.inner.forEach((error) => {
-      if(!error.path) return;
+      if (!error.path) return;
 
       validationErros[error.path] = error.message;
     });
 
     return res.status(400).json({ error: validationErros });
   }
-}
+};
 
 export { middlewareValidadorBodyAdotante };
--- src/middleware/validadores/enderecoRequestBody.ts
+++ src/middleware/validadores/enderecoRequestBody.ts
@@ -1,30 +1,37 @@
-import { Request, Response, NextFunction } from 'express';
-import * as yup from 'yup';
-import EnderecoEntity from '../../entities/Endereco';
+import { Request, Response, NextFunction } from "express";
+import * as yup from "yup";
+import { pt } from "yup-locale-pt";
+import EnderecoEntity from "../../entities/Endereco";
 
-const esquemaBodyEndereco: yup.ObjectSchema<Omit<EnderecoEntity, "id">> = yup.object({
-  cidade: yup.string().defined().required(),
-  estado: yup.string().defined().required(),
-});
+yup.setLocale(pt);
 
-const middlewareValidadorBodyEndereco = async (req: Request, res: Response, next: NextFunction) => {
+const esquemaBodyEndereco: yup.ObjectSchema<Omit<EnderecoEntity, "id">> =
+  yup.object({
+    cidade: yup.string().defined().required(),
+    estado: yup.string().defined().required(),
+  });
+
+const middlewareValidadorBodyEndereco = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     await esquemaBodyEndereco.validate(req.body, { abortEarly: false });
     return next();
-    
   } catch (error) {
     const yupError = error as yup.ValidationError;
 
     const validationErros: Record<string, string> = {};
 
     yupError.inner.forEach((error) => {
-      if(!error.path) return;
+      if (!error.path) return;
 
       validationErros[error.path] = error.message;
     });
 
     return res.status(400).json({ error: validationErros });
   }
-}
+};
 
 export { middlewareValidadorBodyEndereco };
