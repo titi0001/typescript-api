21f4e5b66a0d113a0e1cb7477dcd8c58ced61306
Thiago Pereira <titiura@gmail.com>
2024-07-17 16:48:00 -0300

    feat: install  express async erros

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,26 @@
-import express, { Response } from "express";
+import "express-async-errors"; 
+import express from "express";
 import router from "./routes";
-import  "reflect-metadata"
+import "reflect-metadata";
 import { AppDataSource } from "./config/dataSource";
-
+import { erroMiddleware } from "./middleware/erro";
 
 const app = express();
 app.use(express.json());
 router(app);
 
-AppDataSource.initialize().then(() => { 
-  console.log("Conectado ao banco de dados");
-}
-).catch((error) => {
-  console.log(error);
-});
+app.get("/teste", () => {
+  throw new Error("Erro ao acessar a rota");
+})
 
+app.use(erroMiddleware);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Conectado ao banco de dados");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 export default app;
