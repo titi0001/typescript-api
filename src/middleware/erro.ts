0b3bd65d8f42254830b05c9152ac04e8c99331a6
Thiago Pereira <titiura@gmail.com>
2024-07-17 16:49:26 -0300

    feat: create middleware errors status code

---
+++ src/middleware/erro.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { ManipulaErros } from "../utils/manipulaErros";
+import { EnumHttpStatusCode } from "../enum/EnumHttpStatusCode";
+
+export const erroMiddleware = (
+  erro: ManipulaErros,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode =
+    erro.statusCode ?? EnumHttpStatusCode.INTERNAL_SERVER_ERROR;
+
+  const menssagem = erro.statusCode ? erro.message : "Erro interno do servidor";
+
+  res.status(statusCode).json({ menssagem });
+
+  return next();
+};
