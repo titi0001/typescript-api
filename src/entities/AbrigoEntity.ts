09f86677a0e496644ed4b92678faf30308664d6b
Thiago Pereira <titiura@gmail.com>
2024-07-17 23:23:15 -0300

    feat: create Abrigo entity

---
+++ src/entities/AbrigoEntity.ts
@@ -0,0 +1,49 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import EnderecoEntity from "./Endereco";
+import { criaSenhaCriptografada } from "../utils/senhaCriptografada";
+import PetEntity from "./PetEntity";
+
+@Entity()
+export default class AbrigoEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  @Column()
+  nome: string;
+  @Column()
+  senha: string;
+  @Column()
+  email: string;
+  @Column()
+  celular: string;
+  @OneToOne(() => EnderecoEntity, {
+    nullable: true,
+    cascade: true,
+    eager: true,
+  })
+  @JoinColumn()
+  endereco?: EnderecoEntity;
+  @OneToOne(() => PetEntity, (pet) => pet.abrigo)
+  pets!: PetEntity[];
+
+  constructor(
+    nome: string,
+    senha: string,
+    email: string,
+    celular: string,
+    endereco?: EnderecoEntity
+  ) {
+    this.nome = nome;
+    this.senha = senha;
+    this.email = email;
+    this.celular = celular;
+    this.endereco = endereco;
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  private async cripografarSenha(senha: string): Promise<void> {
+    if (this.senha) {
+      this.senha = criaSenhaCriptografada(this.senha);
+    }
+  }
+}
